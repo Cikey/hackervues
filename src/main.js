f75b90fc8ecb41b5d84c27712b2c72f32f1333d8
Christian Kulpa <chris@almostintelligent.de>
2021-02-07 18:32:44 +0100

    fix: use createWebHashHistory and set public_path for router too

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
 import TopPosts from './components/TopPosts.vue';
 import NewPosts from './components/NewPosts.vue';
@@ -15,7 +15,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
+  base: process.env.PUBLIC_PATH,
   routes
 });
 
